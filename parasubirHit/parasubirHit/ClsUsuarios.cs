4e2f6086b5fed6e2103b96b6775b69419d9327c2
juanchos2018 <jcarlossenati@gmail.com>
2020-06-02 13:20:29 -0500

    comit de primer comintarios

---
+++ parasubirHit/parasubirHit/ClsUsuarios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace parasubirHit
+{
+    class ClsUsuarios
+    {
+        public int numero { get; set; }
+        public int numero2 { get; set; }
+        public int numero3 { get; set; }
+    }
+}
