a7dcc9c2fbab676a108e339155462cc8ee684ef9
juanchos2018 <jcarlossenati@gmail.com>
2020-06-03 17:49:01 -0500

    cre la clase persona

---
+++ parasubirHit/parasubirHit/ClsPersonas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parasubirHit
+{
+    class ClsPersonas
+    {
+        public int numer2 { get; set; }
+        public int numero3 { get; set; }
+        public int nunero4 { get; set; }
+    }
+}
